e702eda8a2311f0082ada4d793814d1bea624fac
shaikot07 <saifulislamshaikat007@gmail.com>
2024-03-22 00:47:34 +0600

    resoluteAi project backend setup and start operatrion

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+const app =express()
+const port = process.env.PORT || 5000;
+
+// middleWare 
+app.use(cors());
+app.use(express.json());
+
+console.log("thht",process.env.DB_PASS);
+console.log(process.env.DB_USER);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.loifkbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb+srv://<username>:<password>@cluster0.loifkbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // start operation 
+    
+
+
+    const userCollection = client.db('resoluteAiJobProject').collection('users');
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        // insert email if user  doseno't exists 
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+              return res.send({ message: 'User already exists', insertedId: null })
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+  })
+
+  app.get('/users',  async (req, res) => {
+    const result = await userCollection.find().toArray();
+    res.send(result)
+});
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Simple CRUD Is RUNNING")
+})
+app.listen(port,()=>{
+    console.log(`Simple CURD IS Running on port,${port}`);
+})
